8e42df2046d323005173a8bf687e1820a7cdc19c
Vincent Arel-Bundock <varel@umich.edu>
2018-06-04 15:17:50 -0400

    test with shuffled data to ensure

--- tests/testthat/test-spatial-lags.R
+++ tests/testthat/test-spatial-lags.R
@@ -1,10 +1,11 @@
 context('Dyadic spatial lags')
 
-unit = letters[1:3]
-time = 1:2
+unit = letters[1:4]
+time = 1:10
 dyad_time = expand.grid('unit1' = unit, 'unit2' = unit, 'time' = time, stringsAsFactors = FALSE) 
 dyad_time$w = rnorm(nrow(dyad_time))
 dyad_time$y = rnorm(nrow(dyad_time))
+dyad_time = dyad_time[sample(1:nrow(dyad_time), nrow(dyad_time)),] # shuffle data
 
 test_that('specific source', {
     #y_ij = rho * sum w_ik y_kj
@@ -18,7 +19,9 @@ test_that('specific source', {
 	b = dplyr::filter(tmp, unit1 == 'a', unit2 == 'b')$w * 
         dplyr::filter(tmp, unit1 == 'b', unit2 == 'b')$y +
 		dplyr::filter(tmp, unit1 == 'a', unit2 == 'c')$w * 
-        dplyr::filter(tmp, unit1 == 'c', unit2 == 'b')$y 
+        dplyr::filter(tmp, unit1 == 'c', unit2 == 'b')$y +
+		dplyr::filter(tmp, unit1 == 'a', unit2 == 'd')$w * 
+        dplyr::filter(tmp, unit1 == 'd', unit2 == 'b')$y 
 	testthat::expect_equal(a, b)
 })
 
@@ -34,6 +37,8 @@ test_that('specific target', {
 	b = dplyr::filter(tmp, unit1 == 'b', unit2 == 'a')$w * 
         dplyr::filter(tmp, unit1 == 'a', unit2 == 'a')$y +
 		dplyr::filter(tmp, unit1 == 'b', unit2 == 'c')$w * 
-        dplyr::filter(tmp, unit1 == 'a', unit2 == 'c')$y 
+        dplyr::filter(tmp, unit1 == 'a', unit2 == 'c')$y +
+	    dplyr::filter(tmp, unit1 == 'b', unit2 == 'd')$w * 
+        dplyr::filter(tmp, unit1 == 'a', unit2 == 'd')$y 
 	testthat::expect_equal(a, b)
 })
