4b28aa21d3a5544e0daa015405412ab3348c7fed
Vincent Arel-Bundock <varel@umich.edu>
2018-08-12 12:11:52 -0400

    specific source more efficient implementation

--- R/spatial_lag.R
+++ R/spatial_lag.R
@@ -29,7 +29,7 @@ sanity_check_dyad_time = function(dyad_time,
     }
 }
 
-#' Produce a new Wy column to measure specific source/target contagion
+#' Add a new Wy column to measure specific source/target contagion
 #' (cross-sectional data)
 #'
 #' @param dat dyad-year dataset (data.frame)
@@ -37,161 +37,53 @@ sanity_check_dyad_time = function(dyad_time,
 #' @param unit_target name of target id column (character)
 #' @param w name of distance/weights column (character)
 #' @param y name of outcome to lag (character)
-#' @param type "source" or "target"-specific contagion (character)
-#' @param zero_diag should the W matrix diagonal be zeroed? (boolean)
 #' @param row_normalize should the W matrix be row-normalized? (boolean)
 #'
 #' @export
-sl_dyad_specific = function(dat,
+specific_source = function(dat,
                    unit_src = 'unit1',
                    unit_tar = 'unit2',
                    w = 'w',
                    y = 'y',
-                   type = 'source',
-                   zero_diag = TRUE,
                    row_normalize = TRUE) {
-    tmp = dat[, c(unit_src, unit_tar, w, y)] 
-    if (type == 'source') {
-        tmp = tmp[order(tmp[, unit_tar], tmp[, unit_src]),]
-    } else if (type == 'target') {
-        tmp = tmp[order(tmp[, unit_src], tmp[, unit_tar]),]
-    } else {
-        stop('type must be "source" or "target"')
-    }
-    W = tmp[, 1:3] %>%
-        tidyr::spread(2, 3) %>%
-        tibble::column_to_rownames(unit_src) %>%
-        as.matrix %>%
-        Matrix::Matrix()
-    W = rep(list(W), nrow(tmp) / nrow(W)) %>%
-        Matrix::bdiag()
-    if (zero_diag) {
-        Matrix::diag(W) = 0
-    }
-    if (row_normalize) {
-        W = W / Matrix::rowSums(W)
-    }
-    tmp$wy = W %*% tmp[, y] %>% as.vector
-    out = tmp[, c(unit_src, unit_tar, 'wy')]
-    return(out)
+    dat = dat[order(dat[, unit_src], dat[, unit_tar]),]
+	w_ik = Matrix::Matrix(dat[, w], nrow=sqrt(nrow(dat)), byrow=TRUE)
+    y_ik = Matrix::Matrix(dat[, y], nrow=sqrt(nrow(dat)), byrow=TRUE)
+	diag(w_ik) = 0
+	if (row_normalize) {
+		w_ik = w_ik / Matrix::rowSums(w_ik)
+	}
+    wy = w_ik %*% y_ik
+    dat$wy = as.vector(Matrix::t(wy))
+    return(dat)
 }
 
-#' Produce a new Wy column to measure specific source/target contagion (panel
-#' data)
+#' A wrapper around `specific_source`. Split-Apply-Combine by time period.
 #'
 #' @param dat dyad-year dataset (data.frame)
 #' @param unit_src name of source id column (character)
 #' @param unit_target name of target id column (character)
-#' @param time name of time id column (character)
+#' @param time name of time column (character)
 #' @param w name of distance/weights column (character)
 #' @param y name of outcome to lag (character)
-#' @param type "source" or "target"-specific contagion (character)
-#' @param zero_diag should the W matrix diagonal be zeroed? (boolean)
 #' @param row_normalize should the W matrix be row-normalized? (boolean)
 #'
 #' @export
-sl_dyad_specific_panel = function(dat, unit_src, unit_tar, time, w, y, type,
-                                  zero_diag, row_normalize) {
-    sanity_check_dyad_time(dat, unit_src, unit_tar, time, w, y)
-    tmp = dat[order(dat[, time]),]
-    idx = tmp[, time]
-    tmp = split(tmp, tmp[, time])
-    out = lapply(tmp, sl_dyad_specific, 
-                 unit_src = unit_src,
-                 unit_tar = unit_tar,
-                 w = w,
-                 y = y,
-                 type = type,
-                 zero_diag = zero_diag,
-                 row_normalize = row_normalize
-                 )
+specific_source_panel = function(dat,
+                                 unit_src = 'unit1',
+                                 unit_tar = 'unit2',
+                                 time = 'time',
+                                 w = 'w',
+                                 y = 'y',
+                                 row_normalize = TRUE) {
+    out = dat[order(dat[, unit_src], dat[, unit_tar], dat[, time]), ]
+    out = split(dat, dat[, time])
+    out = lapply(out, function(x) specific_source(dat = x,
+                                                  unit_src = unit_src,
+                                                  unit_tar = unit_tar,
+                                                  w = w,
+                                                  y = y,
+                                                  row_normalize = row_normalize))
     out = do.call('rbind', out)
-    out[, time] = idx
-    out = out[, c(unit_src, unit_tar, time, 'wy')]
     return(out)
 }
-
-#w_monadic = function(unit_time, dyad_time, y = 'x', w = 'w', row_normalize = TRUE) {
-    ## sanity checks
-    #sanity_check(unit_time, dyad_time, w, y)
-    ## hardcoded variable names
-    #unit_time = unit_time[, c('unit', 'time', y)] %>%
-                 #setNames(c('unit', 'time', 'y'))
-    #dyad_time = dyad_time[, c('unit1', 'unit2', 'time', w)] %>%
-                #setNames(c('unit1', 'unit2', 'time', 'w'))
-	## sorting
-    #unit_time = unit_time %>% 
-                 #dplyr::arrange(time, unit)
-	#dyad_time = dyad_time %>% 
-				#dplyr::arrange(time, unit1, unit2)
-	## W matrix
-    #W = dyad_time %>% 
-        #split(.$time) %>%
-        #purrr::map(~ dplyr::select(., -time)) %>%
-        #purrr::map(~ tidyr::spread(., unit1, w)) %>%
-        #purrr::map(~ tibble::column_to_rownames(., 'unit2')) %>%
-        #purrr::map(as.matrix) %>%
-        #Matrix::bdiag(.)
-    #diag(W) = 0
-	## row normalization
-    #if (row_normalize) {
-        #W = W / rowSums(W)
-    #}
-	## spatial lag
-    #out = W %*% unit_time$y %>%
-          #as.vector
-    #return(out)
-#}
-
-#sanity_check = function(unit_time, dyad_time, w, y) {
-    ## is nrow(dyad_time) a multiple of w
-    #if (!w %in% colnames(dyad_time)) {
-        #stop('w must be a column in dyad_time')
-    #}
-    #if (!y %in% colnames(unit_time)) {
-        #stop('y must be a column in unit_time')
-    #}
-    #if (class(unit_time)[1] != 'data.frame') {
-        #stop('unit_time must be a data.frame')
-    #}
-    #if (class(dyad_time)[1] != 'data.frame') {
-        #stop('dyad_time must be a data.frame')
-    #}
-    #tmp = paste(dyad_time$unit1, dyad_time$unit2, dyad_time$time)
-    #if (anyDuplicated(tmp) != 0) {
-        #stop('Duplicate unit1-unit2-time indices in dyad_time.')
-    #}
-    #tmp = paste(unit_time$unit, unit_time$time)
-    #if (anyDuplicated(tmp) != 0) {
-        #stop('Duplicate unit-time indices in unit_time.')
-    #}
-    #tmp = length(unique(unit_time$unit)) *
-          #length(unique(unit_time$time)) 
-    #if (tmp != nrow(unit_time)) {
-        #stop('unit_time is not rectangular')
-    #}
-    #tmp = length(unique(dyad_time$unit1)) *
-          #length(unique(dyad_time$unit2)) *
-          #length(unique(dyad_time$time)) 
-    #if (tmp != nrow(dyad_time)) {
-        #stop('dyad_time is not rectangular')
-    #}
-    #if (any(is.na(unit_time))) {
-        #stpo('unit_time must not contain missing values')
-    #}
-    #if (any(is.na(dyad_time))) {
-        #stop('dyad_time must not contain missing values')
-    #}
-    #if (length(setdiff(unit_time$unit, dyad_time$unit1)) > 0) {
-        #stop('There are elements in unit_time$unit that do not appear in dyad_time$unit1')
-    #}
-    #if (length(setdiff(unit_time$unit, dyad_time$unit2) > 0)) {
-        #stop('There are elements in unit_time$unit that do not appear in dyad_time$unit2')
-    #}
-    #if (length(setdiff(dyad_time$unit1, unit_time$unit) > 0)) {
-        #stop('There are elements in dyad_time$unit1 that do not appear in unit_time$unit')
-    #}
-    #if (length(setdiff(dyad_time$unit2, unit_time$unit) > 0)) {
-        #stop('There are elements in dyad_time$unit2 that do not appear in unit_time$unit')
-    #}
-#}
--- tests/testthat/test-spatial-lags.R
+++ tests/testthat/test-spatial-lags.R
@@ -1,44 +1,78 @@
 context('Dyadic spatial lags')
 
-unit = letters[1:4]
-time = 1:10
-dyad_time = expand.grid('unit1' = unit, 'unit2' = unit, 'time' = time, stringsAsFactors = FALSE) 
-dyad_time$w = rnorm(nrow(dyad_time))
-dyad_time$y = rnorm(nrow(dyad_time))
-dyad_time = dyad_time[sample(1:nrow(dyad_time), nrow(dyad_time)),] # shuffle data
-
-test_that('specific source', {
-    #y_ij = rho * sum w_ik y_kj
-    #k != i
-	tmp = sl_dyad_specific_panel(dyad_time, 'unit1', 'unit2', 'time', 'w', 'y',
-		                         type = 'source', row_normalize = FALSE, zero_diag = TRUE)
-	tmp = dyad_time %>% 
-          dplyr::left_join(tmp, by = c('unit1', 'unit2', 'time'))
-	tmp = tmp[tmp$time == 2, ]
+test_that('specific source cross-sectional', {
+	unit = letters[1:4]
+	time = 1:10
+	dat = expand.grid('unit1' = unit, 'unit2' = unit, stringsAsFactors = FALSE) 
+	dat$w = rnorm(nrow(dat))
+	dat$y = rnorm(nrow(dat))
+	dat = dat[sample(1:nrow(dat), nrow(dat)),] # shuffle data
+	tmp = specific_source(dat, row_normalize = FALSE)
 	a = dplyr::filter(tmp, unit1 == 'a', unit2 == 'b')$wy
-	b = dplyr::filter(tmp, unit1 == 'a', unit2 == 'b')$w * 
-        dplyr::filter(tmp, unit1 == 'b', unit2 == 'b')$y +
-		dplyr::filter(tmp, unit1 == 'a', unit2 == 'c')$w * 
-        dplyr::filter(tmp, unit1 == 'c', unit2 == 'b')$y +
-		dplyr::filter(tmp, unit1 == 'a', unit2 == 'd')$w * 
-        dplyr::filter(tmp, unit1 == 'd', unit2 == 'b')$y 
+	b = dplyr::filter(tmp, unit1 == 'a', unit2 == 'b')$w * dplyr::filter(tmp, unit1 == 'b', unit2 == 'b')$y +
+		dplyr::filter(tmp, unit1 == 'a', unit2 == 'c')$w * dplyr::filter(tmp, unit1 == 'c', unit2 == 'b')$y +
+		dplyr::filter(tmp, unit1 == 'a', unit2 == 'd')$w * dplyr::filter(tmp, unit1 == 'd', unit2 == 'b')$y 
 	testthat::expect_equal(a, b)
 })
 
-test_that('specific target', {
-    #y_ij = rho * sum w_jk y_ik
-    #k != j
-	tmp = sl_dyad_specific_panel(dyad_time, 'unit1', 'unit2', 'time', 'w', 'y', 
-						         type = 'target', row_normalize = FALSE, zero_diag = TRUE)
-	tmp = dyad_time %>% 
-          dplyr::left_join(tmp, by = c('unit1', 'unit2', 'time'))
-	tmp = tmp[tmp$time == 2, ]
+test_that('specific source panel', {
+	unit = letters[1:4]
+	time = 1:10
+	dat = expand.grid('unit1' = unit, 'unit2' = unit, 'time' = time, stringsAsFactors = FALSE) 
+	dat$w = rnorm(nrow(dat))
+	dat$y = rnorm(nrow(dat))
+	dat = dat[sample(1:nrow(dat), nrow(dat)),] # shuffle data
+	tmp = specific_source_panel(dat, row_normalize = FALSE)
+    tmp = tmp[tmp$time == 9,]
 	a = dplyr::filter(tmp, unit1 == 'a', unit2 == 'b')$wy
-	b = dplyr::filter(tmp, unit1 == 'b', unit2 == 'a')$w * 
-        dplyr::filter(tmp, unit1 == 'a', unit2 == 'a')$y +
-		dplyr::filter(tmp, unit1 == 'b', unit2 == 'c')$w * 
-        dplyr::filter(tmp, unit1 == 'a', unit2 == 'c')$y +
-	    dplyr::filter(tmp, unit1 == 'b', unit2 == 'd')$w * 
-        dplyr::filter(tmp, unit1 == 'a', unit2 == 'd')$y 
+	b = dplyr::filter(tmp, unit1 == 'a', unit2 == 'b')$w * dplyr::filter(tmp, unit1 == 'b', unit2 == 'b')$y +
+		dplyr::filter(tmp, unit1 == 'a', unit2 == 'c')$w * dplyr::filter(tmp, unit1 == 'c', unit2 == 'b')$y +
+		dplyr::filter(tmp, unit1 == 'a', unit2 == 'd')$w * dplyr::filter(tmp, unit1 == 'd', unit2 == 'b')$y 
 	testthat::expect_equal(a, b)
 })
+
+#test_that('specific source', {
+    ##y_ij = rho * sum w_ik y_kj
+    ##k != i
+	#tmp = sl_dyad_specific_panel(dyad_time, 'unit1', 'unit2', 'time', 'w', 'y',
+								 #type = 'source', row_normalize = FALSE, zero_diag = TRUE)
+	#tmp = dyad_time %>% 
+          #dplyr::left_join(tmp, by = c('unit1', 'unit2', 'time'))
+	#tmp = tmp[tmp$time == 2, ]
+	#a = dplyr::filter(tmp, unit1 == 'a', unit2 == 'b')$wy
+	#b = dplyr::filter(tmp, unit1 == 'a', unit2 == 'b')$w * 
+        #dplyr::filter(tmp, unit1 == 'b', unit2 == 'b')$y +
+		#dplyr::filter(tmp, unit1 == 'a', unit2 == 'c')$w * 
+        #dplyr::filter(tmp, unit1 == 'c', unit2 == 'b')$y +
+		#dplyr::filter(tmp, unit1 == 'a', unit2 == 'd')$w * 
+        #dplyr::filter(tmp, unit1 == 'd', unit2 == 'b')$y 
+	#testthat::expect_equal(a, b)
+#})
+
+#test_that('specific target', {
+    ##y_ij = rho * sum w_jk y_ik
+    ##k != j
+	#tmp = sl_dyad_specific_panel(dyad_time, 'unit1', 'unit2', 'time', 'w', 'y', 
+								 #type = 'target', row_normalize = FALSE, zero_diag = TRUE)
+	#tmp = dyad_time %>% 
+          #dplyr::left_join(tmp, by = c('unit1', 'unit2', 'time'))
+	#tmp = tmp[tmp$time == 2, ]
+	#a = dplyr::filter(tmp, unit1 == 'a', unit2 == 'b')$wy
+	#b = dplyr::filter(tmp, unit1 == 'b', unit2 == 'a')$w * 
+        #dplyr::filter(tmp, unit1 == 'a', unit2 == 'a')$y +
+		#dplyr::filter(tmp, unit1 == 'b', unit2 == 'c')$w * 
+        #dplyr::filter(tmp, unit1 == 'a', unit2 == 'c')$y +
+		#dplyr::filter(tmp, unit1 == 'b', unit2 == 'd')$w * 
+        #dplyr::filter(tmp, unit1 == 'a', unit2 == 'd')$y 
+	#testthat::expect_equal(a, b)
+#})
+
+
+#unit = letters[1:4]
+#time = 1:10
+#dyad_time = expand.grid('unit1' = unit, 'unit2' = unit, 'time' = time, stringsAsFactors = FALSE) 
+#dyad_time$w = rnorm(nrow(dyad_time))
+#dyad_time$y = rnorm(nrow(dyad_time))
+#dyad_time$w = 1:16
+#dyad_time$y = 
+#dyad_time = dyad_time[sample(1:nrow(dyad_time), nrow(dyad_time)),] # shuffle data
